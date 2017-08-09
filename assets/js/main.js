c76a66d6e793089985faabd6afa5b938a9091aaa
GLADYSYOVANAMAMANICAMATICONA <roxsy.lj@gmail.com>
2017-08-09 11:09:27 -0500

    imagenes con javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,9 @@
-var pictures = ["assets/img/a1.jpg","assets/img/a2.jpg", "assets/img/a3.jpg", "assets/img/a4.jpg"];
- 
+		var pictures = ["assets/img/a1.jpg","assets/img/a2.jpg", "assets/img/a3.jpg", "assets/img/a4.jpg"];
+
+		for (var i = 0; i < pictures.length; i++) {
+			var imagen = document.createElement('img');//creo la etiqueta img  en imagen...
+				  imagen.setAttribute('class', 'cssimagen');//creo atributo para poder llamar en css por la clase ___.o....
+			      imagen.src = pictures[i];
+			      imagenes.appendChild(imagen);
+		}
+		 
